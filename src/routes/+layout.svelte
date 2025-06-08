c99438073d099ad91e615489c1335853ecffc89f
Russell <miller.time.baby@gmail.com>
2025-06-08 11:30:15 -0700

    update main page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,15 @@
 <script lang="ts">
-	import '../app.css';
-	
+	import "../app.css";
+
 	let { children } = $props();
 </script>
 
-{@render children()}
+<div class="grid grid-rows-[auto_1fr_auto]">
+	<header class="bg-surface-900 p-4">Smart Sync</header>
+
+	<div
+		class="container mx-auto grid grid-cols-1 xl:grid-cols-[200px_minmax(0px,_1fr)_200px]"
+	>
+		{@render children()}
+	</div>
+</div>
